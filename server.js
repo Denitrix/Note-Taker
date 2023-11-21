87447b6f8be42d820af79b1576795f7a63c7b090
Alex Majorowicz <alexmajors0613@gmail.com>
2023-11-20 21:16:26 -0600

    add middleware for json and urlencoded

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-/* // Middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); */
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 app.get(
   //sends index.html to root path
